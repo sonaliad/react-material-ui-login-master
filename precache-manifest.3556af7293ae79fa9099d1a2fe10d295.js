2e413347fa152dc336a94e0e0d717a422d98e4e4
Sonali Purwar <>
2021-10-13 08:53:21 +0530

    Updates

---
+++ precache-manifest.3556af7293ae79fa9099d1a2fe10d295.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd903cdd5e765504d9c1ce954f46abdc",
+    "url": "/sonaliad/react-material-ui-login-master/index.html"
+  },
+  {
+    "revision": "ee5a1856cb0792597215",
+    "url": "/sonaliad/react-material-ui-login-master/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "16d9ee3f6a1b3073f0be",
+    "url": "/sonaliad/react-material-ui-login-master/static/js/2.cd2897db.chunk.js"
+  },
+  {
+    "revision": "ee5a1856cb0792597215",
+    "url": "/sonaliad/react-material-ui-login-master/static/js/main.4c79c7f6.chunk.js"
+  },
+  {
+    "revision": "2a18305bdfbbcb2ce492",
+    "url": "/sonaliad/react-material-ui-login-master/static/js/runtime-main.f4f9538e.js"
+  }
+]);
